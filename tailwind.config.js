2985c516a0f780ba0f6331c61d7d02a39b4e5a3b
datRobotuit <22520241@gm.uit.edu.vn>
2025-03-30 15:10:27 +0700

    adjust nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        text: ["Poppins", 'sans-serif']
+        text: ['Montserrat','sans-serif']
       },
       colors: {
         primary: '#A3804A',
-        // secondary: '#FFF671',
+        secondary: '#FFF671',
         descText: '#4D4D4D',
         black: '#000000',
         white: '#ffffff',
